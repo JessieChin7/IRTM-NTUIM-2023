fd2cfaadb3f0430f8d73f44b9b181540feac74ac
JessieChin7 <yuan2001@live.com>
2023-10-08 23:42:38 +0800

    :sparkles: (pa2) complete pa2

--- PA1/pa1.py
+++ PA1/pa1.py
@@ -1,77 +1,35 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "['yugoslav', 'author', 'plan', 'arrest', 'eleven', 'coal', 'miner', 'opposit', 'politician', 'suspicion', 'sabotag', 'connect', 'strike', 'action', 'presid', 'slobodan', 'milosev', 'listen', 'bbc', 'news', 'world']\n"
-     ]
-    }
-   ],
-   "source": [
-    "from nltk.stem import PorterStemmer\n",
-    "\n",
-    "# Load the stopwords from a file\n",
-    "# https://gist.github.com/larsyencken/1440509\n",
-    "with open('stopwords.txt', 'r') as f:\n",
-    "    stop_words = set(f.read().splitlines())\n",
-    "\n",
-    "# Load the input text\n",
-    "text = \"\"\"And Yugoslav authorities are planning the arrest of eleven coal miners \n",
-    "and two opposition politicians on suspicion of sabotage, that's in \n",
-    "connection with strike action against President Slobodan Milosevic. \n",
-    "You are listening to BBC news for The World.\"\"\"\n",
-    "\n",
-    "# Step 1: Tokenization using basic Python functions (without external library)\n",
-    "# Remove punctuations and split the text into words\n",
-    "translator = str.maketrans('', '', '''!\"#$%&'()*+,-./:;<=>?@[\\\\]^_`{|}~''')\n",
-    "tokens = text.translate(translator).split()\n",
-    "\n",
-    "# Step 2: Lowercasing\n",
-    "tokens = [word.lower() for word in tokens]\n",
-    "\n",
-    "# Step 3: Stemming using Porter’s algorithm\n",
-    "ps = PorterStemmer()\n",
-    "tokens = [ps.stem(word) for word in tokens]\n",
-    "\n",
-    "# Step 4: Stopword Removal using the loaded stopwords set\n",
-    "tokens = [word for word in tokens if word not in stop_words]\n",
-    "\n",
-    "# Step 5: Save the result as a txt file\n",
-    "with open('result.txt', 'w') as f:\n",
-    "    for token in tokens:\n",
-    "        f.write(\"%s\\n\" % token)\n",
-    "\n",
-    "# Print out the tokens for verification\n",
-    "print(tokens)"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.10.11"
-  },
-  "orig_nbformat": 4
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+from nltk.stem import PorterStemmer
+
+# Load the stopwords from a file
+# https://gist.github.com/larsyencken/1440509
+with open('stopwords.txt', 'r') as f:
+    stop_words = set(f.read().splitlines())
+
+# Load the input text
+text = """And Yugoslav authorities are planning the arrest of eleven coal miners 
+and two opposition politicians on suspicion of sabotage, that's in 
+connection with strike action against President Slobodan Milosevic. 
+You are listening to BBC news for The World."""
+
+# Step 1: Tokenization using basic Python functions (without external library)
+# Remove punctuations and split the text into words
+translator = str.maketrans('', '', '''!"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~''')
+tokens = text.translate(translator).split()
+
+# Step 2: Lowercasing
+tokens = [word.lower() for word in tokens]
+
+# Step 3: Stopword Removal using the loaded stopwords set
+tokens = [word for word in tokens if word not in stop_words]
+
+# Step 4: Stemming using Porter’s algorithm
+ps = PorterStemmer()
+tokens = [ps.stem(word) for word in tokens]
+
+# Step 5: Save the result as a txt file
+with open('result.txt', 'w') as f:
+    for token in tokens:
+        f.write("%s\n" % token)
+
+# Print out the tokens for verification
+print(tokens)
